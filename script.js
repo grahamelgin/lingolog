56a8eb07bc63837f3d19547c9395c7f7c061a69e
grahamelginclemson <gelgin@clemson.edu>
2026-01-14 16:27:01 -0500

    Removed all files for restart

--- script.js
+++
@@ -1,7 +0,0 @@
-let sessions = [];
-
-// On page load, attempt to load existing sessions from localStorage
-const saved = localStorage.getItem("sessions");
-if (saved) {
-    sessions = JSON.parse(saved);
-}
